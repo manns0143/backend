2bc8c7002dd2c1c385cd3bcd122a0ecbf1b97e5e
Mann <shahmann0143@gmail.com>
2025-02-11 15:06:36 +0530

    add util code files

--- project/src/index.js
+++ project/src/index.js
@@ -1,11 +1,24 @@
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
+import { app } from "./app.js";
 
-dotenv.config({
-    path: "./.env"
-});
+dotenv.config();
 
-connectDB();
+const port = process.env.PORT || 8000;
+connectDB()
+.then(() => {
+    app.on("error", (error) => {
+        console.error("server error: ", error);
+        process.exit(1);
+    })
+    app.listen(port, () => {
+        console.log("server is listening on port: ", port);
+    })
+})
+.catch((error) => {
+    console.error("mongodb connection failed: ", error);
+    process.exit(1);
+})
 
 // import mongoose from "mongoose";
 // import { DB_NAME } from "src/constants.js";
